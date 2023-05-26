3df492d9bb219635152c279ef5e8bad135ec5ed8
Rik Hennekens <r.hennekens@student.fontys.nl>
2023-05-26 10:19:42 +0200

    .

--- main.go
+++ main.go
@@ -8,27 +8,54 @@ import (
 	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+
 func main() {
-	OpenDB()
-	readInput()
-	getUserData(inputUsername)
-	passwordControl(inputPassword, password)
-
-}
-
-// Databaseverbinding opzetten
-func OpenDB() (*sql.DB, error) {
-	db, err := sql.Open("mysql", "rik:SQLR1k@tcp(localhost:3306)/Login system DB")
+	// Databaseverbinding opzetten
+	var err error
+	db, err = sql.Open("mysql", "rik:SQLR1k@tcp(localhost:3306)/Login system DB")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
+
+	// Lees de gebruikersinvoer
+	inputUsername, inputPassword := readInput()
+
+	// Haal gebruikersgegevens op
+	username, password := getUserData(inputUsername)
+
+	// Controleer de gebruikersnaam
+	usernameControl(inputUsername, username)
+
+	// Controleer het ingevoerde wachtwoord
+	passwordControl(inputPassword, password)
+
+	fmt.Println(username, password)
 }
 
-// Lees de gebruikersinvoer
-func readInput() {
+func CreateTable() {
+	// Tabel maken als deze nog niet bestaat
+	var err error
+	createTableQuery := `
+	CREATE TABLE IF NOT EXISTS users (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		username VARCHAR(255) NOT NULL,
+		password VARCHAR(255) NOT NULL
+	);`
+	_, err = db.Exec(createTableQuery)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Tabel 'users' is gemaakt of bestaat al.")
+}
+
+// Lees de gebruikersinvoer en retourneer deze
+func readInput() (string, string) {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Gebruikersnaam: ")
 	inputUsername, _ := reader.ReadString('\n')
@@ -38,28 +65,40 @@ func readInput() {
 	// Verwijder nieuwe regels van de ingevoerde waarden
 	inputUsername = strings.TrimSpace(inputUsername)
 	inputPassword = strings.TrimSpace(inputPassword)
+
+	return inputUsername, inputPassword
 }
 
-// Query om gebruikersgegevens op te halen
-func getUserData(inputUsername string) {
+// Query om gebruikersgegevens op te halen en retourneer ze
+func getUserData(inputUsername string) (string, string) {
 	query := "SELECT * FROM users WHERE username=?"
 	row := db.QueryRow(query, inputUsername)
 
 	var username, password string
-	err = row.Scan(&username, &password)
+	err := row.Scan(&username, &password)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			fmt.Println("Ongeldige gebruikersnaam of wachtwoord.")
 		} else {
 			panic(err)
 		}
-		return
+		return "", ""
 	}
+
+	return username, password
 }
 
 // Controleer het ingevoerde wachtwoord
 func passwordControl(inputPassword string, password string) {
 	if inputPassword == password {
+		fmt.Println("Inloggen gelukt!")
+	} else {
+		fmt.Println("Ongeldige gebruikersnaam of wachtwoord.")
+	}
+}
+
+func usernameControl(inputUsername string, username string) {
+	if inputUsername == username {
 		fmt.Println("Inloggen gelukt!")
 	} else {
 		fmt.Println("Ongeldige gebruikersnaam of wachtwoord.")
