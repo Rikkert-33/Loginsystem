87bee4a6eddcd891cd269078b0c8ca1532ca1cbc
Rik Hennekens <r.hennekens@student.fontys.nl>
2023-06-01 13:11:20 +0200

    login works

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
-	_ "github.com/lib/pq"
 )
 
 var db *sql.DB
@@ -16,42 +15,61 @@ var db *sql.DB
 func main() {
 	// Databaseverbinding opzetten
 	var err error
-	db, err = sql.Open("mysql", "rik:SQLR1k@tcp(localhost:3306)/mijndb")
+	db, err = sql.Open("mysql", "rik:SQLR1k@tcp(localhost:3306)/mijndb2")
 	if err != nil {
-		fmt.Println("Fout bij het openen van de database.")
+		fmt.Println("Fout bij het openen van de database.", err)
+		return
 	}
 	defer db.Close()
+
 	// Tabel maken als deze nog niet bestaat
 	CreateTable()
 
-	// Lees de gebruikersinvoer
-	inputUsername, inputPassword := readInput()
+	var choice int
+	fmt.Println("Welkom bij de inlogapplicatie! Wilt u inloggen of registreren?")
+	fmt.Println("Type 1 voor inloggen of 2 voor registreren:")
+	fmt.Scanln(&choice)
 
-	// Haal gebruikersgegevens op
-	username, password := getUserData(inputUsername)
+	if choice == 1 {
+		// Lees de gebruikersinvoer
+		inputUsername, inputPassword := readInput()
+		// Controleer de gebruikersnaam en het wachtwoord
+		err := checkCredentials(inputUsername, inputPassword)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("Inloggen gelukt!")
+	} else if choice == 2 {
+		// Lees de gebruikersinvoer
+		inputUsername, inputPassword := readInput()
 
-	// Controleer de gebruikersnaam
-	usernameControl(inputUsername, username)
+		// Voeg de gebruiker toe aan de database
+		err := addUser(inputUsername, inputPassword)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("Gebruiker is toegevoegd aan de database.")
+	} else {
+		fmt.Println("Ongeldige keuze.")
+	}
 
-	// Controleer het ingevoerde wachtwoord
-	passwordControl(inputPassword, password)
-
-	fmt.Println(username, password)
 }
 
-func CreateTable() {
+func CreateTable() error {
 	// Tabel maken als deze nog niet bestaat
-	var err error
 	createTableQuery := `
 	CREATE TABLE IF NOT EXISTS users (
 		id INT AUTO_INCREMENT PRIMARY KEY,
 		username VARCHAR(255) NOT NULL,
 		password VARCHAR(255) NOT NULL
 	);`
-	_, err = db.Exec(createTableQuery)
+	_, err := db.Exec(createTableQuery)
 	if err != nil {
-		fmt.Println("Fout bij het maken van de tabel.", err)
+		return err
 	}
+	return nil
 }
 
 // Lees de gebruikersinvoer en retourneer deze
@@ -69,39 +87,67 @@ func readInput() (string, string) {
 	return inputUsername, inputPassword
 }
 
-// Query om gebruikersgegevens op te halen en retourneer ze
-func getUserData(inputUsername string) (string, string) {
-	query := "SELECT * FROM users WHERE username=?"
-	row := db.QueryRow(query, inputUsername)
+// Voeg een gebruiker toe aan de database
+func addUser(username, password string) error {
+	// Controleer of de gebruiker al bestaat in de database
+	query := "SELECT COUNT(*) FROM users WHERE username=?"
+	row := db.QueryRow(query, username)
 
-	var username, password string
-	err := row.Scan(&username, &password)
+	var count int
+	err := row.Scan(&count)
 	if err != nil {
-		if err == sql.ErrNoRows {
-			fmt.Println("Gebruiker niet gevonden.")
-		} else {
-			fmt.Println("Fout bij het ophalen van de gebruikersgegevens.")
-		}
-		return "", ""
+		return err
 	}
 
-	return username, password
+	if count > 0 {
+		return fmt.Errorf("gebruikersnaam is al in gebruik")
+	}
+
+	// Voeg de gebruiker toe aan de database
+	insertQuery := "INSERT INTO users (username, password) VALUES (?, ?)"
+	_, err = db.Exec(insertQuery, username, password)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-// Controleer het ingevoerde wachtwoord
-func passwordControl(inputPassword string, password string) {
-	if inputPassword == password {
-		fmt.Println("Inloggen gelukt!")
-	} else {
-		fmt.Println("Ongeldig wachtwoord.")
-	}
-}
+// Query om gebruikersgegevens op te halen en retourneer ze
+// func getUserData(inputUsername string) (inputPassword, string) (string, string) {
+// 	query := "SELECT username, password FROM users WHERE username=?"
+// 	row := db.QueryRow(query, inputUsername, inputPassword)
 
-// Controleer de gebruikersnaam
-func usernameControl(inputUsername string, username string) {
-	if inputUsername == username {
-		fmt.Println("Inloggen gelukt!")
-	} else {
-		fmt.Println("Ongeldige gebruikersnaam.")
+// 	var username, password string
+// 	err := row.Scan(&username, &password)
+// 	if err != nil {
+// 		if err == sql.ErrNoRows {
+// 			fmt.Println("Gebruiker niet gevonden.")
+// 		} else {
+// 			fmt.Println("Fout bij het ophalen van de gebruikersgegevens.", err)
+// 		}
+// 		return "", ""
+// 	}
+
+// 	fmt.Println("Inloggen gelukt!")
+// 	return username, password
+
+// }
+
+// Controleer de gebruikersnaam en het wachtwoord
+func checkCredentials(username, password string) error {
+	query := "SELECT COUNT(*) FROM users WHERE username=? AND password=?"
+	row := db.QueryRow(query, username, password)
+
+	var count int
+	err := row.Scan(&count)
+	if err != nil {
+		return err
 	}
+
+	if count == 0 {
+		return fmt.Errorf("ongeldige gebruikersnaam of wachtwoord")
+	}
+
+	return nil
 }
