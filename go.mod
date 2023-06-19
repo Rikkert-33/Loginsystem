81bac08a2d4ea7ae2dfe40ac7552453ca2344d52
Rik Hennekens <r.hennekens@student.fontys.nl>
2023-06-19 14:09:00 +0200

    deployment changes

--- go.mod
+++ go.mod
@@ -3,9 +3,6 @@ module github.com/Rikkert-33/Loginsystem
 go 1.16
 
 require (
-	github.com/gin-gonic/gin v1.9.1
-	github.com/go-playground/validator/v10 v10.14.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.1
-	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 
 )
--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"bufio"
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"html/template"
+	"log"
 	"net/http"
 	"os"
-	"strings"
 
-	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -22,126 +20,97 @@ type Config struct {
 	DBport string `json:"dbport"`
 }
 
-var db *sql.DB
+var (
+	db        *sql.DB
+	templates *template.Template
+)
 
 func main() {
-	//Read the config file
-	config, err := loadConfig("config.json")
+	// Read the config file
+	file, err := os.Open("config.json")
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	config := Config{}
+	err = decoder.Decode(&config)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	// Databaseverbinding opzetten
+	// Connect to the database
 	dsn := fmt.Sprintf("%s:%s@/%s", config.DBuser, config.DBpass, config.DBname)
 	db, err = sql.Open("mysql", dsn)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
 	defer db.Close()
 
-	// Tabel maken als deze nog niet bestaat
-	CreateTable()
+	// Create the necessary database table if it doesn't exist
+	if err := CreateTable(); err != nil {
+		log.Fatal(err)
+	}
 
-	var choice int
-	fmt.Println("Welkom bij de inlogapplicatie! Wilt u inloggen of registreren?")
-	fmt.Println("Type 1 voor inloggen of 2 voor registreren:")
-	fmt.Scanln(&choice)
+	// Load HTML templates
+	templates = template.Must(template.ParseGlob("templates/*.html"))
 
-	if choice == 1 {
-		// Lees de gebruikersinvoer
-		inputUsername, inputPassword := readInput()
-		// Controleer de gebruikersnaam en het wachtwoord
-		err := checkCredentials(inputUsername, inputPassword)
-		if err != nil {
-			fmt.Println(err)
+	// Register HTTP handlers
+	http.HandleFunc("/", handleIndex)
+	http.HandleFunc("/login", handleLogin)
+	http.HandleFunc("/register", handleRegister)
+
+	// Start the web server
+	log.Println("Server is running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	if err := templates.ExecuteTemplate(w, "index.html", nil); err != nil {
+		log.Println("error:", err)
+	}
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		if err := checkCredentials(username, password); err == nil {
+			fmt.Fprintln(w, "Login successful!")
 			return
 		}
-		fmt.Println("Inloggen gelukt!")
-	} else if choice == 2 {
-		// Lees de gebruikersinvoer
-		inputUsername, inputPassword := readInput()
 
-		// Voeg de gebruiker toe aan de database
-		err := addUser(inputUsername, inputPassword)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println("Gebruiker is toegevoegd aan de database.")
-	} else {
-		fmt.Println("Ongeldige keuze.")
-	}
-
-	// create the gin router
-	router := gin.Default()
-
-	// define the route
-	router.GET("/", indexHandler)
-	router.POST("/login", loginHandler)
-	router.POST("/register", registerHandler)
-
-	// run the server
-	router.Run("localhost:8080")
-
-}
-
-// LoadConfig loads the configuration from a JSON file
-func loadConfig(filename string) (Config, error) {
-	config := Config{}
-
-	data, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return config, err
-	}
-
-	err = json.Unmarshal(data, &config)
-	if err != nil {
-		return config, err
-	}
-
-	return config, nil
-}
-
-func indexHandler(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.html", nil)
-}
-
-func loginHandler(c *gin.Context) {
-	username := c.PostForm("username")
-	password := c.PostForm("password")
-
-	// Check the credentials
-	err := checkCredentials(username, password)
-	if err != nil {
-		c.HTML(http.StatusOK, "login.html", gin.H{
-			"Error": err.Error(),
-		})
+		fmt.Fprintln(w, "Invalid username or password.")
 		return
 	}
 
-	c.HTML(http.StatusOK, "success.html", nil)
+	if err := templates.ExecuteTemplate(w, "login.html", nil); err != nil {
+		log.Println("error:", err)
+	}
 }
 
-func registerHandler(c *gin.Context) {
-	username := c.PostForm("username")
-	password := c.PostForm("password")
+func handleRegister(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
 
-	// Add the user to the database
-	err := addUser(username, password)
-	if err != nil {
-		c.HTML(http.StatusOK, "register.html", gin.H{
-			"Error": err.Error(),
-		})
+		if err := addUser(username, password); err == nil {
+			fmt.Fprintln(w, "Registration successful!")
+			return
+		}
+
+		fmt.Fprintln(w, "Username is already in use.")
 		return
 	}
 
-	c.HTML(http.StatusOK, "success.html", nil)
+	if err := templates.ExecuteTemplate(w, "register.html", nil); err != nil {
+		log.Println("error:", err)
+	}
 }
 
 func CreateTable() error {
-	// Tabel maken als deze nog niet bestaat
 	createTableQuery := `
 	CREATE TABLE IF NOT EXISTS users (
 		id INT AUTO_INCREMENT PRIMARY KEY,
@@ -153,21 +122,6 @@ func CreateTable() error {
 		return err
 	}
 	return nil
-}
-
-// Lees de gebruikersinvoer en retourneer deze
-func readInput() (string, string) {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Gebruikersnaam: ")
-	inputUsername, _ := reader.ReadString('\n')
-	fmt.Print("Wachtwoord: ")
-	inputPassword, _ := reader.ReadString('\n')
-
-	// Verwijder nieuwe regels van de ingevoerde waarden
-	inputUsername = strings.TrimSpace(inputUsername)
-	inputPassword = strings.TrimSpace(inputPassword)
-
-	return inputUsername, inputPassword
 }
 
 // Voeg een gebruiker toe aan de database
